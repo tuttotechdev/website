4ced1bca3c319a9364653b032b5bafd809f3d6b8
rh363 <alex.massaroni2004@gmail.com>
2025-02-25 21:18:55 +0100

    First scatch of DB structure

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,76 @@
 from django.contrib import admin
 
-# Register your models here.
+from website.models import Category, Product, Feedback, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ["id", "product", "image"]
+
+    search_fields = ["product__name"]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    readonly_fields = ["id"]
+    search_fields = ["id", "name"]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "slug",
+        "created_at",
+        "updated_at",
+        "short_description",
+        "cost",
+        "clicks",
+        "views",
+        "purchased",
+    ]
+
+    inlines = [ProductImageInline]
+
+    readonly_fields = ["id", "clicks", "views", "created_at", "updated_at"]
+
+    search_fields = [
+        "id",
+        "name",
+        "slug",
+        "short_description",
+        "short_description_it",
+        "description",
+        "description_it",
+        "labels_it",
+        "labels",
+        "categories__name",
+        "categories__slug",
+    ]
+
+    list_filter = ["categories", "is_active", "is_featured"]
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "product", "rating"]
+    readonly_fields = ["user", "product", "feedback", "rating"]
+
+    search_fields = [
+        "user__username",
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+        "feedback",
+        "product__name",
+        "product__slug",
+    ]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,75 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    name_it = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    slug = models.SlugField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    name_it = models.CharField(max_length=100)
+
+    cost = models.FloatField()
+
+    labels = models.JSONField(blank=True, null=True)
+    labels_it = models.JSONField(blank=True, null=True)
+
+    short_description = models.TextField()
+    short_description_it = models.TextField()
+
+    description = models.TextField()
+    description_it = models.TextField()
+
+    rating = models.FloatField()
+
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    revenue = models.IntegerField(default=0)
+    purchased = models.IntegerField(default=0)
+
+    affiliate_link = models.URLField(blank=True, null=True)
+
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    categories = models.ManyToManyField(Category, blank=True, related_name="products")
+
+    see_also = models.ManyToManyField("self", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(
+        upload_to="products/images/%Y/%m/%d", blank=True, null=True
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+
+class Feedback(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    feedback = models.TextField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}({self.user.email}) {self.product.name}"
